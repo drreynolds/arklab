129597c35669883f249fbde167a3acb26d455d94
Daniel R. Reynolds <reynolds@smu.edu>
2019-09-30 13:32:01 -0500

    Moved 'F' and 'A' DIRK and ARK routines inside solve_DIRK and solve_ARK function files

--- solve_ARK.m
+++ solve_ARK.m
@@ -199,7 +199,7 @@ for tstep = 2:length(tvals)
       nsteps = nsteps + 1;
       
       % compute new solution (and embedding if available)
-      [Ynew,Y2] = Y_DIRK(z,Fdata);
+      [Ynew,Y2] = Y_ARK(z,Fdata);
 
       % if stages succeeded and time step adaptivity enabled, check step accuracy
       if ((st_fail == 0) & embedded)
@@ -263,15 +263,14 @@ for tstep = 2:length(tvals)
    
 end  % time step loop
 
-% end solve_DIRK function
+% end solve_ARK function
 end
 
 
 
 
-
-function [y,y2] = Y_DIRK(z, Fdata)
-% usage: [y,y2] = Y_DIRK(z, Fdata)
+function [y,y2] = Y_ARK(z, Fdata)
+% usage: [y,y2] = Y_ARK(z, Fdata)
 %
 % Inputs:
 %    z     = stage solutions [z1, ..., zs]
@@ -305,7 +304,7 @@ end
 [zrows,zcols] = size(z);
 nvar = zrows;
 if (zcols ~= s)
-   error('Y_DIRK error: z has incorrect number of stages');
+   error('Y_ARK error: z has incorrect number of stages');
 end
 
 % call RHS at our stages
@@ -323,6 +322,79 @@ end
 %    ynew = yold + h*sum(be(j)*fe(j) + bi(j)*fi(j))
 y  = Fdata.yold + Fdata.h*fe*be + Fdata.h*fi*bi;
 y2 = Fdata.yold + Fdata.h*fe*be2 + Fdata.h*fi*bi2;
+
+% end of function
+end
+
+
+
+
+function F = F_ARK(z, Fdata)
+% usage: F = F_ARK(z, Fdata)
+%
+% Inputs:  z = current guess for stage solution
+%          Fdata = structure containing extra information for evaluating F.
+% Outputs: F = residual at current guess
+%
+% This function computes the (non)linear residuals for an intermediate
+% stage solution, through calling the user-supplied (in Fdata) ODE
+% right-hand side function.
+%
+% Daniel R. Reynolds
+% Department of Mathematics
+% Southern Methodist University
+% March 2017
+% All Rights Reserved
+
+% extract ARK method information from Fdata
+Bi = Fdata.Bi;
+[Brows, Bcols] = size(Bi);
+s  = Bcols - 1;
+ci = Bi(1:s,1);
+Ai = Bi(1:s,2:s+1);
+h  = Fdata.h;
+st = Fdata.stage;
+t  = Fdata.t + Fdata.h*ci(st);
+
+% form the ARK residual
+%    F = z - rhs - h*(ai(stage,stage)*fstage)
+F = z - Fdata.rhs - h*Ai(st,st)*Fdata.fi(t, z);
+
+% end of function
+end
+
+
+
+
+function Amat = A_ARK(z, Fdata)
+% usage: Amat = A_ARK(z, Fdata)
+%
+% Inputs:  z = current guess for stage solution
+%          Fdata = structure containing extra information for evaluating F.
+% Outputs: Amat = Jacobian at current guess
+%
+% This function computes the Jacobian of each intermediate stage residual
+% for a multi-stage ARK method, through calling the user-supplied (in
+% Fdata) ODE Jacobian function. 
+%
+% Daniel R. Reynolds
+% Department of Mathematics
+% Southern Methodist University
+% March 2017
+% All Rights Reserved
+
+% extract ARK method information from Fdata
+Bi = Fdata.Bi;
+[Brows, Bcols] = size(Bi);
+s   = Bcols - 1;
+ci  = Bi(1:s,1);
+bi  = (Bi(s+1,2:s+1))';
+Ai  = Bi(1:s,2:s+1);
+st  = Fdata.stage;
+t   = Fdata.t + Fdata.h*ci(st);
+
+% form the ARK Jacobian
+Amat = eye(length(z)) - Fdata.h*Ai(st,st)*Fdata.Ji(t, z);
 
 % end of function
 end
--- solve_DIRK.m
+++ solve_DIRK.m
@@ -294,3 +294,74 @@ y2 = Fdata.yold + Fdata.h*f*b2;
 end
 
 
+
+
+function F = F_DIRK(z, Fdata)
+% usage: F = F_DIRK(z, Fdata)
+%
+% Inputs:  z = current guess for stage solution
+%          Fdata = structure containing extra information for evaluating F.
+% Outputs: F = residual at current guess
+%
+% This function computes the (non)linear residuals for an intermediate
+% stage solution, through calling the user-supplied (in Fdata) ODE
+% right-hand side function.
+%
+% Daniel R. Reynolds
+% Department of Mathematics
+% Southern Methodist University
+% March 2017
+% All Rights Reserved
+
+% extract DIRK method information from Fdata
+B = Fdata.B;
+[Brows, Bcols] = size(B);
+s  = Bcols - 1;
+c  = B(1:s,1);
+A  = B(1:s,2:s+1);
+h  = Fdata.h;
+st = Fdata.stage;
+t  = Fdata.t + Fdata.h*c(st);
+
+% form the DIRK residual
+%    F = z - rhs - h*(a(stage,stage)*fstage)
+F = z - Fdata.rhs - h*A(st,st)*Fdata.f(t, z);
+
+% end of function
+end
+
+
+
+
+function Amat = A_DIRK(z, Fdata)
+% usage: Amat = A_DIRK(z, Fdata)
+%
+% Inputs:  z = current guess for stage solution
+%          Fdata = structure containing extra information for evaluating F.
+% Outputs: Amat = Jacobian at current guess
+%
+% This function computes the Jacobian of each intermediate stage residual
+% for a multi-stage DIRK method, through calling the user-supplied (in
+% Fdata) ODE Jacobian function. 
+%
+% Daniel R. Reynolds
+% Department of Mathematics
+% Southern Methodist University
+% March 2017
+% All Rights Reserved
+
+% extract DIRK method information from Fdata
+B = Fdata.B;
+[Brows, Bcols] = size(B);
+s  = Bcols - 1;
+c  = B(1:s,1);
+b  = (B(s+1,2:s+1))';
+A  = B(1:s,2:s+1);
+st = Fdata.stage;
+t  = Fdata.t + Fdata.h*c(st);
+
+% form the DIRK Jacobian
+Amat = eye(length(z)) - Fdata.h*A(st,st)*Fdata.J(t, z);
+
+% end of function
+end
