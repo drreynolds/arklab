272fed4a6dee5de651e657e04f012cfc3b8e518d
Daniel R. Reynolds <reynolds@smu.edu>
2019-12-06 08:51:50 -0600

    Created alternate approach for time-dependent boundary condition enforcement

--- driver_timedep_bdry2.m
+++ driver_timedep_bdry2.m
@@ -46,7 +46,7 @@ Y0 = zeros(size(Pdata.xspan));
 DIRKmethods = {'SDIRK-2-2','EDIRK-3-3','Kvaerno(5,3,4)-ESDIRK','Kvaerno(7,4,5)-ESDIRK'};
 ARKEmethods = {'ARK(2,3,2)-ERK','Ascher(2,3,3)-ERK','ARK4(3)7L[2]SA-ERK','ARK5(4)8L[2]SA-ERK'};
 ARKImethods = {'ARK(2,3,2)-SDIRK','Ascher(2,3,3)-SDIRK','ARK4(3)7L[2]SA-ESDIRK','ARK5(4)8L[2]SA-ESDIRK'};
-ERKmethods  = {'ERK-1-1','ERK-2-2','ARK(2,3,2)-ERK','ERK-4-4','Dormand-Prince-ERK'};
+ERKmethods  = {'ERK-1-1','ERK-2-2','Ascher(2,3,3)-ERK','ERK-4-4','Dormand-Prince-ERK'};
 
 
 % general output header information
@@ -57,26 +57,35 @@ fprintf('    tolerances:  rtol = %g,  atol = %g\n', rtol, atol(1));
 
 
 % create 'statistics' storage
-DIRK_recommended_rmserrs = zeros(length(lambdas),length(DIRKmethods),length(hvals));
-DIRK_recommended_ord = zeros(length(lambdas),length(DIRKmethods));
-DIRK_recommended_ordred = zeros(length(lambdas),length(DIRKmethods));
+DIRK_natural_rmserrs = zeros(length(lambdas),length(DIRKmethods),length(hvals));
+DIRK_natural_ord = zeros(length(lambdas),length(DIRKmethods));
+DIRK_natural_ordred = zeros(length(lambdas),length(DIRKmethods));
 DIRK_forced_rmserrs = zeros(length(lambdas),length(DIRKmethods),length(hvals));
 DIRK_forced_ord = zeros(length(lambdas),length(DIRKmethods));
 DIRK_forced_ordred = zeros(length(lambdas),length(DIRKmethods));
+DIRK_approx_rmserrs = zeros(length(lambdas),length(DIRKmethods),length(hvals));
+DIRK_approx_ord = zeros(length(lambdas),length(DIRKmethods));
+DIRK_approx_ordred = zeros(length(lambdas),length(DIRKmethods));
 
-ARK_recommended_rmserrs = zeros(length(lambdas),length(ARKEmethods),length(hvals));
-ARK_recommended_ord = zeros(length(lambdas),length(ARKEmethods));
-ARK_recommended_ordred = zeros(length(lambdas),length(ARKEmethods));
+ARK_natural_rmserrs = zeros(length(lambdas),length(ARKEmethods),length(hvals));
+ARK_natural_ord = zeros(length(lambdas),length(ARKEmethods));
+ARK_natural_ordred = zeros(length(lambdas),length(ARKEmethods));
 ARK_forced_rmserrs = zeros(length(lambdas),length(ARKEmethods),length(hvals));
 ARK_forced_ord = zeros(length(lambdas),length(ARKEmethods));
 ARK_forced_ordred = zeros(length(lambdas),length(ARKEmethods));
+ARK_approx_rmserrs = zeros(length(lambdas),length(ARKEmethods),length(hvals));
+ARK_approx_ord = zeros(length(lambdas),length(ARKEmethods));
+ARK_approx_ordred = zeros(length(lambdas),length(ARKEmethods));
 
-ERK_recommended_rmserrs = zeros(length(lambdas),length(ERKmethods),length(hvals));
-ERK_recommended_ord = zeros(length(lambdas),length(ERKmethods));
-ERK_recommended_ordred = zeros(length(lambdas),length(ERKmethods));
+ERK_natural_rmserrs = zeros(length(lambdas),length(ERKmethods),length(hvals));
+ERK_natural_ord = zeros(length(lambdas),length(ERKmethods));
+ERK_natural_ordred = zeros(length(lambdas),length(ERKmethods));
 ERK_forced_rmserrs = zeros(length(lambdas),length(ERKmethods),length(hvals));
 ERK_forced_ord = zeros(length(lambdas),length(ERKmethods));
 ERK_forced_ordred = zeros(length(lambdas),length(ERKmethods));
+ERK_approx_rmserrs = zeros(length(lambdas),length(ERKmethods),length(hvals));
+ERK_approx_ord = zeros(length(lambdas),length(ERKmethods));
+ERK_approx_ordred = zeros(length(lambdas),length(ERKmethods));
 
 % loop over stiffness parameters
 for il = 1:length(lambdas)
@@ -87,7 +96,7 @@ for il = 1:length(lambdas)
    tout = linspace(T0,Tf,11);
    Pdata.lambda = lambda;
    fprintf('\nStiffness factor lambda = %g\n', lambda);
-   
+
    % set problem-defining functions  (t must be scalar in each)
    fn = @f_timedep_bdry;
    fe = @fe_timedep_bdry;
@@ -117,19 +126,19 @@ for il = 1:length(lambdas)
                       + cos(xr*4)*(Pdata.c(4))*(exp(-Pdata.lambda*t*16)) ...
                       + cos(xr*5)*(Pdata.c(5))*(exp(-Pdata.lambda*t*25)) );
 
-   % generate reference solution 
+   % generate reference solution
    fprintf('\nGenerating reference solution with ode15s:\n')
    opts = odeset('RelTol',1e-13, 'AbsTol',atol,'Jacobian',Jn);
    [t,Yref] = ode15s(fn, tout, Y0, opts);
    Yref = Yref';
 
-   % solve with 'recommended' approach
-   fprintf('\n  Results with recommended approach:\n')
+   % solve with 'natural' approach
+   fprintf('\n  Results with natural approach:\n')
 
    % run DIRK tests
-   if (length(DIRKmethods) > 0) 
+   if (length(DIRKmethods) > 0)
       for ib = 1:length(DIRKmethods)
-         
+
          mname = DIRKmethods{ib};
          B = butcher(mname);  s = numel(B(1,:))-1;  q = B(s+1,1);
          fprintf('\n    DIRK integrator: %s (order = %i)\n',mname,q)
@@ -143,8 +152,8 @@ for il = 1:length(lambdas)
             if (isnan(errs(ih)) || isinf(errs(ih)) || errs(ih) > 10)
                errs(ih) = 1;
             end
-            DIRK_recommended_rmserrs(il,ib,ih) = errs(ih);
-            if (ih>1) 
+            DIRK_natural_rmserrs(il,ib,ih) = errs(ih);
+            if (ih>1)
                order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
                fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
             else
@@ -154,16 +163,16 @@ for il = 1:length(lambdas)
          end
          s = sort(order);  ord = s(end-1);
          fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
-         DIRK_recommended_ord(il,ib) = ord;
-         DIRK_recommended_ordred(il,ib) = max(0,q-ord);
-         
+         DIRK_natural_ord(il,ib) = ord;
+         DIRK_natural_ordred(il,ib) = max(0,q-ord);
+
       end
    end
 
    % run ARK tests
    if ((length(ARKImethods) == length(ARKEmethods)) && (length(ARKImethods)>0))
       for ib = 1:length(ARKImethods)
-         
+
          mname1 = ARKEmethods{ib};
          Be = butcher(mname1);  s = numel(Be(1,:))-1;  q = Be(s+1,1);
          mname2 = ARKImethods{ib};
@@ -179,8 +188,8 @@ for il = 1:length(lambdas)
             if (isnan(errs(ih)) || isinf(errs(ih)) || errs(ih) > 10)
                errs(ih) = 1;
             end
-            ARK_recommended_rmserrs(il,ib,ih) = errs(ih);
-            if (ih>1) 
+            ARK_natural_rmserrs(il,ib,ih) = errs(ih);
+            if (ih>1)
                order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
                fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
             else
@@ -190,18 +199,18 @@ for il = 1:length(lambdas)
          end
          s = sort(order);  ord = s(end-1);
          fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
-         ARK_recommended_ord(il,ib) = ord;
-         ARK_recommended_ordred(il,ib) = max(0,q-ord);
-    
+         ARK_natural_ord(il,ib) = ord;
+         ARK_natural_ordred(il,ib) = max(0,q-ord);
+
       end
    end
-   
+
    % run ERK tests
    if ((length(ERKmethods) > 0) && (abs(lambda)<=100))
       for ib = 1:length(ERKmethods)
          mname = ERKmethods{ib};
          B = butcher(mname);  s = numel(B(1,:))-1;  q = B(s+1,1);
-         fprintf('\n  ERK integrator: %s (order = %i)\n',mname,q)
+         fprintf('\n   ERK integrator: %s (order = %i)\n',mname,q)
          errs_rms = zeros(size(hvals));
          errs_max = zeros(size(hvals));
          order = zeros(length(hvals)-1,1);
@@ -213,8 +222,8 @@ for il = 1:length(lambdas)
             if (isnan(errs(ih)) || isinf(errs(ih)) || errs(ih) > 10)
                errs(ih) = 1;
             end
-            ARK_recommended_rmserrs(il,ib,ih) = errs(ih);
-            if (ih>1) 
+            ARK_natural_rmserrs(il,ib,ih) = errs(ih);
+            if (ih>1)
                order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
                fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
             else
@@ -224,12 +233,12 @@ for il = 1:length(lambdas)
          end
          s = sort(order);  ord = s(end-1);
          fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
-         ERK_recommended_ord(il,ib) = ord;
-         ERK_recommended_ordred(il,ib) = max(0,q-ord);
-         
+         ERK_natural_ord(il,ib) = ord;
+         ERK_natural_ordred(il,ib) = max(0,q-ord);
+
       end
    end
-   
+
    % solve with 'forced' approach
    fprintf('\n  Results with forced approach:\n')
 
@@ -239,24 +248,12 @@ for il = 1:length(lambdas)
    fi = @fi_timedep_bdry2;
    Jn = @J_timedep_bdry2;
    Ji = @J_timedep_bdry2;
-   Es = @(t,y) Tf;
-   k = 1:length(Pdata.c);
-   Pdata.b1 = @(t) ( cos(xl*1)*(Pdata.c(1))/(Pdata.lambda*1)*(1-exp(-Pdata.lambda*t)) ...
-                   + cos(xl*2)*(Pdata.c(2))/(Pdata.lambda*4)*(1-exp(-Pdata.lambda*t*4)) ...
-                   + cos(xl*3)*(Pdata.c(3))/(Pdata.lambda*9)*(1-exp(-Pdata.lambda*t*9)) ...
-                   + cos(xl*4)*(Pdata.c(4))/(Pdata.lambda*16)*(1-exp(-Pdata.lambda*t*16)) ...
-                   + cos(xl*5)*(Pdata.c(5))/(Pdata.lambda*25)*(1-exp(-Pdata.lambda*t*25)) );
-   Pdata.b2 = @(t) ( cos(xr*1)*(Pdata.c(1))/(Pdata.lambda*1)*(1-exp(-Pdata.lambda*t)) ...
-                   + cos(xr*2)*(Pdata.c(2))/(Pdata.lambda*4)*(1-exp(-Pdata.lambda*t*4)) ...
-                   + cos(xr*3)*(Pdata.c(3))/(Pdata.lambda*9)*(1-exp(-Pdata.lambda*t*9)) ...
-                   + cos(xr*4)*(Pdata.c(4))/(Pdata.lambda*16)*(1-exp(-Pdata.lambda*t*16)) ...
-                   + cos(xr*5)*(Pdata.c(5))/(Pdata.lambda*25)*(1-exp(-Pdata.lambda*t*25)) );
    bdry = @enforce_timedep_bdry2;
-   
+
    % run DIRK tests
-   if (length(DIRKmethods) > 0) 
+   if (length(DIRKmethods) > 0)
       for ib = 1:length(DIRKmethods)
-         
+
          mname = DIRKmethods{ib};
          B = butcher(mname);  s = numel(B(1,:))-1;  q = B(s+1,1);
          fprintf('\n    DIRK integrator: %s (order = %i)\n',mname,q)
@@ -271,7 +268,7 @@ for il = 1:length(lambdas)
                errs(ih) = 1;
             end
             DIRK_forced_rmserrs(il,ib,ih) = errs(ih);
-            if (ih>1) 
+            if (ih>1)
                order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
                fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
             else
@@ -283,14 +280,14 @@ for il = 1:length(lambdas)
          fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
          DIRK_forced_ord(il,ib) = ord;
          DIRK_forced_ordred(il,ib) = max(0,q-ord);
-         
+
       end
    end
 
    % run ARK tests
    if ((length(ARKImethods) == length(ARKEmethods)) && (length(ARKImethods)>0))
       for ib = 1:length(ARKImethods)
-         
+
          mname1 = ARKEmethods{ib};
          Be = butcher(mname1);  s = numel(Be(1,:))-1;  q = Be(s+1,1);
          mname2 = ARKImethods{ib};
@@ -307,7 +304,7 @@ for il = 1:length(lambdas)
                errs(ih) = 1;
             end
             ARK_forced_rmserrs(il,ib,ih) = errs(ih);
-            if (ih>1) 
+            if (ih>1)
                order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
                fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
             else
@@ -319,16 +316,16 @@ for il = 1:length(lambdas)
          fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
          ARK_forced_ord(il,ib) = ord;
          ARK_forced_ordred(il,ib) = max(0,q-ord);
-    
+
       end
    end
-   
+
    % run ERK tests
    if ((length(ERKmethods) > 0) && (abs(lambda)<=100))
       for ib = 1:length(ERKmethods)
          mname = ERKmethods{ib};
          B = butcher(mname);  s = numel(B(1,:))-1;  q = B(s+1,1);
-         fprintf('\n  ERK integrator: %s (order = %i)\n',mname,q)
+         fprintf('\n   ERK integrator: %s (order = %i)\n',mname,q)
          errs_rms = zeros(size(hvals));
          errs_max = zeros(size(hvals));
          order = zeros(length(hvals)-1,1);
@@ -341,7 +338,7 @@ for il = 1:length(lambdas)
                errs(ih) = 1;
             end
             ARK_forced_rmserrs(il,ib,ih) = errs(ih);
-            if (ih>1) 
+            if (ih>1)
                order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
                fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
             else
@@ -353,10 +350,146 @@ for il = 1:length(lambdas)
          fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
          ERK_forced_ord(il,ib) = ord;
          ERK_forced_ordred(il,ib) = max(0,q-ord);
-         
+
       end
    end
-   
+
+
+   % solve with 'approximate' approach
+   fprintf('\n  Results with approx approach:\n')
+
+   % set problem-defining functions  (t must be scalar in each)
+   fn = @f_timedep_bdry3;
+   fe = @fe_timedep_bdry3;
+   fi = @fi_timedep_bdry;
+   Jn = @J_timedep_bdry;
+   Ji = @J_timedep_bdry;
+
+   % run DIRK tests
+   if (length(DIRKmethods) > 0)
+      for ib = 1:length(DIRKmethods)
+
+         mname = DIRKmethods{ib};
+         B = butcher(mname);  s = numel(B(1,:))-1;  q = B(s+1,1);
+         fprintf('\n    DIRK integrator: %s (order = %i)\n',mname,q)
+         errs = zeros(size(hvals));
+         order = zeros(length(hvals)-1,1);
+         for ih = 1:length(hvals)
+            hval = hvals(ih)/lambda;
+            fprintf('      h = %.5e,',hval);
+            global bDotApprox
+            bDotApprox.nstored = 0;
+            bDotApprox.nmax = 3;
+            bDotApprox.b1 = zeros(bDotApprox.nmax,1);
+            bDotApprox.b2 = zeros(bDotApprox.nmax,1);
+            bDotApprox.t = zeros(bDotApprox.nmax,1);
+            bDotApprox.h = hval;
+            [t,Y,ns,nl,cf,af] = solve_DIRK(fn, Jn, tout, Y0, B, rtol, atol, hval, hval, 0);
+            errs(ih) = sqrt(sum(sum((Y-Yref).^2))/numel(Y));
+            if (isnan(errs(ih)) || isinf(errs(ih)) || errs(ih) > 10)
+               errs(ih) = 1;
+            end
+            DIRK_approx_rmserrs(il,ib,ih) = errs(ih);
+            if (ih>1)
+               order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
+               fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
+            else
+               fprintf('  rmserr = %.2e,  lsolves = %i\n',errs(ih),nl);
+            end
+            fprintf('      newton conv fails = %i, temporal error fails = %i\n',cf,af);
+         end
+         s = sort(order);  ord = s(end-1);
+         fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
+         DIRK_approx_ord(il,ib) = ord;
+         DIRK_approx_ordred(il,ib) = max(0,q-ord);
+
+      end
+   end
+
+   % run ARK tests
+   if ((length(ARKImethods) == length(ARKEmethods)) && (length(ARKImethods)>0))
+      for ib = 1:length(ARKImethods)
+
+         mname1 = ARKEmethods{ib};
+         Be = butcher(mname1);  s = numel(Be(1,:))-1;  q = Be(s+1,1);
+         mname2 = ARKImethods{ib};
+         Bi = butcher(mname2);
+         fprintf('\n    ARK integrator: %s/%s (order = %i)\n',mname1,mname2,q)
+         errs = zeros(size(hvals));
+         order = zeros(length(hvals)-1,1);
+         for ih = 1:length(hvals)
+            hval = hvals(ih)/lambda;
+            fprintf('      h = %.5e,',hval);
+            global bDotApprox
+            bDotApprox.nstored = 0;
+            bDotApprox.nmax = 3;
+            bDotApprox.b1 = zeros(bDotApprox.nmax,1);
+            bDotApprox.b2 = zeros(bDotApprox.nmax,1);
+            bDotApprox.t = zeros(bDotApprox.nmax,1);
+            bDotApprox.h = hval;
+            [t,Y,ns,nl,cf,af] = solve_ARK(fe, fi, Ji, tout, Y0, Be, Bi, rtol, atol, hval, hval, 0);
+            errs(ih) = sqrt(sum(sum((Y-Yref).^2))/numel(Y));
+            if (isnan(errs(ih)) || isinf(errs(ih)) || errs(ih) > 10)
+               errs(ih) = 1;
+            end
+            ARK_approx_rmserrs(il,ib,ih) = errs(ih);
+            if (ih>1)
+               order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
+               fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
+            else
+               fprintf('  rmserr = %.2e,  lsolves = %i\n',errs(ih),nl);
+            end
+            fprintf('      newton conv fails = %i, temporal error fails = %i\n',cf,af);
+         end
+         s = sort(order);  ord = s(end-1);
+         fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
+         ARK_approx_ord(il,ib) = ord;
+         ARK_approx_ordred(il,ib) = max(0,q-ord);
+
+      end
+   end
+
+   % run ERK tests
+   if ((length(ERKmethods) > 0) && (abs(lambda)<=100))
+      for ib = 1:length(ERKmethods)
+         mname = ERKmethods{ib};
+         B = butcher(mname);  s = numel(B(1,:))-1;  q = B(s+1,1);
+         fprintf('\n   ERK integrator: %s (order = %i)\n',mname,q)
+         errs_rms = zeros(size(hvals));
+         errs_max = zeros(size(hvals));
+         order = zeros(length(hvals)-1,1);
+         for ih = 1:length(hvals)
+            hval = hvals(ih)/lambda/20;
+            fprintf('      h = %.5e,',hval);
+            global bDotApprox
+            bDotApprox.nstored = 0;
+            bDotApprox.nmax = 3;
+            bDotApprox.b1 = zeros(bDotApprox.nmax,1);
+            bDotApprox.b2 = zeros(bDotApprox.nmax,1);
+            bDotApprox.t = zeros(bDotApprox.nmax,1);
+            bDotApprox.h = hval;
+            [t,Y,ns,af] = solve_ERK(fn, Es, tout, Y0, B, rtol, atol, hval, hval);
+            errs(ih) = sqrt(sum(sum((Y-Yref).^2))/numel(Y));
+            if (isnan(errs(ih)) || isinf(errs(ih)) || errs(ih) > 10)
+               errs(ih) = 1;
+            end
+            ARK_approx_rmserrs(il,ib,ih) = errs(ih);
+            if (ih>1)
+               order(ih-1) = log( errs(ih)/errs(ih-1) ) / log( hvals(ih)/hvals(ih-1) );
+               fprintf('  rmserr = %.2e,  lsolves = %i,  order = %.2e\n',errs(ih),nl,order(ih-1));
+            else
+               fprintf('  rmserr = %.2e,  lsolves = %i\n',errs(ih),nl);
+            end
+            fprintf('      temporal error fails = %i\n',af);
+         end
+         s = sort(order);  ord = s(end-1);
+         fprintf('    estimated order = %g,  reduction = %g\n',ord,max(0,q-ord))
+         ERK_approx_ord(il,ib) = ord;
+         ERK_approx_ordred(il,ib) = max(0,q-ord);
+
+      end
+   end
+
 end
 
 
---
+++ fe_timedep_bdry3.m
@@ -0,0 +1,60 @@
+function udot = fe_timedep_bdry3(t, u)
+% usage: udot = fe_timedep_bdry3(t, u)
+%
+% Daniel R. Reynolds
+% Department of Mathematics
+% Southern Methodist University
+% October 2019
+% All Rights Reserved
+
+% extract problem data
+global Pdata;
+c = Pdata.c;
+m = Pdata.m;
+xspan = Pdata.xspan;
+
+% initialize RHS terms
+udot = zeros(m,1);
+
+% interior index set
+int = 2:m-1;
+x = xspan(int);
+
+% forcing term
+udot(int) = c(1)*cos(x*1) + c(2)*cos(x*2) + c(3)*cos(x*3) + c(4)*cos(x*4) + c(5)*cos(x*5);
+
+% handle time-dependent boundary condition approximations
+global bDotApprox
+
+% on the first call, evaluate time-dependent boundary values to fill approximation structure
+if (bDotApprox.nstored == 0)
+  delta = bDotApprox.h/bDotApprox.nmax;
+  tvals = linspace(t-bDotApprox.nmax/2*delta, t+bDotApprox.nmax/2*delta, bDotApprox.nmax);
+  for i=1:bDotApprox.nmax
+    bDotApprox.b1(i) = Pdata.b1(tvals(i));
+    bDotApprox.b2(i) = Pdata.b2(tvals(i));
+    bDotApprox.t(i) = tvals(i);
+  end
+  bDotApprox.nstored = bDotApprox.nmax;
+
+% on other calls, only update approximation structure at new "t" value
+else
+  if (min(abs(t-bDotApprox.t)) > 100*eps)
+    [!,i] = max(abs(t-bDotApprox.t));
+    bDotApprox.b1(i) = Pdata.b1(t);
+    bDotApprox.b2(i) = Pdata.b2(t);
+    bDotApprox.t(i) = t;
+  end
+end
+
+% left boundary
+p = polyfit(bDotApprox.t, bDotApprox.b1, bDotApprox.nmax-1);
+dp = polyder(p);
+udot(1) = polyval(dp,t);
+
+% right boundary
+p = polyfit(bDotApprox.t, bDotApprox.b2, bDotApprox.nmax-1);
+dp = polyder(p);
+udot(m) = polyval(dp,t);
+
+% end function
